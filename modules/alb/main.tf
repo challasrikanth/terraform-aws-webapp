cd54d20e30a88e88a97fa9263f7d15b658b10cb0
srikanth <csri@nuuday.com>
2026-03-02 22:57:00 +0530

    alb modules added

---
+++ modules/alb/main.tf
@@ -0,0 +1,27 @@
+resource "aws_alb" "main" {
+  name= "ha-webapp-alb"
+  load_balancer_type = "application"
+  subnets = var.public_subnets
+
+}
+
+resource "aws.aws_lb_target_group" "tg" {
+  name     = "ha-webapp-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  
+}
+
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}

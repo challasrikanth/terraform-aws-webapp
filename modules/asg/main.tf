3046953b5b89a4e346095ec27650a81920776c22
srikanth <csri@nuuday.com>
2026-03-03 16:47:07 +0530

    added sg

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -3,6 +3,7 @@ resource "aws_launch_template" "lt" {
 
   image_id      = var.ami
   instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.ec2_sg.id ]
 
   user_data = base64encode(<<-EOF
 #!/bin/bash
@@ -30,5 +31,26 @@ resource "aws_autoscaling_group" "asg" {
 }
 
 
+resource "aws_security_group" "ec2_sg" {
+  name        = "webapp-ec2-sg"
+  description = "Allow HTTP from ALB; optional SSH"
+  vpc_id      = var.vpc_id
 
+  # HTTP from ALB only
+  ingress {
+    description              = "HTTP from ALB SG"
+    from_port                = 80
+    to_port                  = 80
+    protocol                 = "tcp"
+    security_groups          = [var.alb_sg_id] 
+   
+  }
 
+  egress {
+    description = "Allow all egress (updates via NAT)"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- modules/asg/variables.tf
+++ modules/asg/variables.tf
@@ -11,4 +11,12 @@ variable "private_subnets" {
 }
 variable "target_group_arn" {
 
+}   
+
+variable "alb_sg_id" {
+  type        = string
+}
+
+variable "vpc_id" {
+  type        = string
 }
